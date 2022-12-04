946758f65e17dfcb04e0954a8b00c008b3940621
username-tbd <username-tbd-0@proton.me>
2022-12-04 11:39:30 -0600

    Clojure: Clean up day 4 names and split function

--- clojure/clj-aoc/src/clj_aoc/day04.clj
+++ clojure/clj-aoc/src/clj_aoc/day04.clj
@@ -6,23 +6,23 @@
   (u/load-lines "../../inputs/input-04.txt"))
 
 (defn split-assignment [assignment]
-  (->> (re-find #"((\d+)-(\d+)),((\d+)-(\d+))" assignment)
-       (#(map % [2 3 5 6]))
-       (map read-string)
+  (->> assignment
+       (re-find #"((\d+)-(\d+)),((\d+)-(\d+))")
+       (#(map (comp read-string %) [2 3 5 6]))
        (split-at 2)))
 
-(defn either-contained? [x]
+(defn containment? [x]
   (let [contained-in?
         (fn [p q] (and (>= (first p) (first q)) (<= (last p) (last q))))]
     (or (contained-in? (first x) (last x))
         (contained-in? (last x) (first x)))))
 
 (defn overlap? [x]
-  (and (>= (second (first x)) (first (second x)))
-       (<= (first (first x)) (second (second x)))))
+  (and (>= (last (first x)) (first (last x)))
+       (<= (first (first x)) (last (last x)))))
 
 (defn count-assignments [pred lines]
   (apply + (map (comp {false 0 true 1} pred split-assignment) lines)))
 
-(println (count-assignments either-contained? lines))
+(println (count-assignments containment? lines))
 (println (count-assignments overlap? lines))
