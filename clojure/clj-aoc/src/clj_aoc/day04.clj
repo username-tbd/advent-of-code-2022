431ade845f29d2cc5bf50ecc50815d3ecf4b6c9e
username-tbd <username-tbd-0@proton.me>
2022-12-04 02:32:10 -0600

    Clojure: Day 4 part 1

---
+++ clojure/clj-aoc/src/clj_aoc/day04.clj
@@ -0,0 +1,20 @@
+(ns clj-aoc.day04
+  (:require [clj-aoc.util :as u])
+  (:gen-class))
+
+(def lines
+  (u/load-lines "../../inputs/input-04.txt"))
+
+(defn split-assignment [assignment]
+  (->> (re-find #"((\d+)-(\d+)),((\d+)-(\d+))" assignment)
+       (#(map % [2 3 5 6]))
+       (map read-string)
+       (split-at 2)))
+
+(defn full-contain? [x]
+  (or (and (>= (first (first x)) (first (second x)))
+             (<= (second (first x)) (second (second x))))
+        (and (<= (first (first x)) (first (second x)))
+             (>= (second (first x)) (second (second x))))))
+
+(apply + (map (comp {false 0 true 1} full-contain? split-assignment) lines))
