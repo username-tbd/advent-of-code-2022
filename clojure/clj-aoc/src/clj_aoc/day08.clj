d67f4fcc78d66c3747ca917b2ebe3b6697b48213
username-tbd <username-tbd-0@proton.me>
2022-12-08 13:59:37 -0600

    clj08: Add part 2

--- clojure/clj-aoc/src/clj_aoc/day08.clj
+++ clojure/clj-aoc/src/clj_aoc/day08.clj
@@ -52,7 +52,54 @@
 
 (->> (reduce combine-flat-vis-seqs flat-vis-seqs)
      (map {false 0 true 1})
-     (apply +))
+     (apply +)
+     println)
 
 ;; -----------
 ;; Part Two
+
+(defn calc-view [tree-line]
+  "Calculate the viewing distance from the first element.
+  (5 2 3 5 1) => 3; (1 5 0 1) => 1; (3) => 0"
+  (let [tree (first tree-line)]
+    (loop [view-line (rest tree-line)
+           view-distance 0]
+      (cond
+        (empty? view-line) view-distance
+        (>= (first view-line) tree) (inc view-distance)
+        :else (recur (rest view-line) (inc view-distance))))))
+
+(defn build-views-line [tree-line]
+  (loop [trees tree-line
+         views-vec []]
+    (let [tree (first trees)]
+      (if (nil? tree)
+        views-vec
+        (recur (rest trees)
+               (conj views-vec (calc-view trees)))))))
+
+;; More natural here to do "view-direction" vs. "view-from"
+(defn build-views-mat [{:keys [tree-mat view-direction]}]
+  (let [build-views-line-reverse
+        (fn [tree-line] ((comp reverse build-views-line reverse) tree-line))]
+    (condp = view-direction
+      :right (map build-views-line tree-mat)
+      :left (map build-views-line-reverse tree-mat)
+      :down (->> (transpose tree-mat)
+                 (map build-views-line)
+                 transpose)
+      :up (->> (transpose tree-mat)
+               (map build-views-line-reverse)
+               transpose))))
+
+(def views-mats
+  (map #(build-views-mat {:tree-mat tree-mat :view-direction %})
+       [:right :left :down :up]))
+
+(def flat-views-seqs
+  (map flatten views-mats))
+
+(def viewing-distance-scores
+  (reduce #(map * %1 %2) flat-views-seqs))
+
+(println (apply max viewing-distance-scores))
