1d65d707cca76658e1600e3a70b529de12946388
username-tbd <username-tbd-0@proton.me>
2022-12-09 09:55:00 -0600

    clj09: Refactor follower move calcs and rename

--- clojure/clj-aoc/src/clj_aoc/day09.clj
+++ clojure/clj-aoc/src/clj_aoc/day09.clj
@@ -15,15 +15,15 @@
        (map move-line->unit-moves-vec) ; Triply nested vectors now
        (reduce concat []))) ; Semi-flattening to a vector of vectors
 
-(defn calc-follower-move [vector-between]
-  (if (some #(> (abs %) 1) vector-between)
-    (mapv #(compare % 0) vector-between)
-    [0 0]))
+(defn calc-follower-move [leader-pos follower-pos]
+  (let [vector-between (mapv - leader-pos follower-pos)]
+    (if (some #(> (abs %) 1) vector-between)
+      (mapv #(compare % 0) vector-between)
+      [0 0])))
 
 (defn perform-move [head-pos tail-pos head-move]
   (let [head-pos-new (mapv + head-pos head-move)
-        vector-between (mapv - head-pos-new tail-pos)
-        tail-move (calc-follower-move vector-between)
+        tail-move (calc-follower-move head-pos-new tail-pos)
         tail-pos-new (mapv + tail-pos tail-move)]
     [head-pos-new tail-pos-new]))
 
@@ -48,14 +48,14 @@
   (loop [old-positions positions
          new-positions []
          move head-move]
-    (let [old-position (first old-positions)
-          new-position (mapv + old-position move)]
+    (let [leader-pos-old (first old-positions)
+          leader-pos-new (mapv + leader-pos-old move)
+          follower-pos-old (second old-positions)]
       (if (empty? (rest old-positions))
-        (conj new-positions (mapv + old-position move))
+        (conj new-positions leader-pos-new)
         (recur (rest old-positions)
-               (conj new-positions new-position)
-               (calc-follower-move
-                 (mapv - new-position (second old-positions))))))))
+               (conj new-positions leader-pos-new)
+               (calc-follower-move leader-pos-new follower-pos-old))))))
 
 (defn find-tail-hits-snake [head-moves]
   (loop [positions (vec (repeat 10 [0 0]))
