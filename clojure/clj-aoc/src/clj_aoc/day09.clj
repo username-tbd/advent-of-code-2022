8b762113c21872cfb3ebfaea9195cdc9270c46f0
username-tbd <username-tbd-0@proton.me>
2022-12-09 13:56:43 -0600

    clj09: Simplify data cleaning

--- clojure/clj-aoc/src/clj_aoc/day09.clj
+++ clojure/clj-aoc/src/clj_aoc/day09.clj
@@ -10,10 +10,10 @@
         base-vec ({"R" [1 0] "L" [-1 0] "U" [0 1] "D" [0 -1]} direction)]
     (vec (repeat magnitude base-vec))))
 
-(def unit-moves ; [[1 0] [0 1] [0 1] [-1 0] etc]
+(def unit-moves ; ([1 0] [0 1] [0 1] [-1 0] etc)
   (->> (u/load-lines 9)
-       (map move-line->unit-moves-vec) ; Triply nested vectors now
-       (reduce concat []))) ; Semi-flattening to a vector of vectors
+       (map move-line->unit-moves-vec) 
+       (reduce concat)))
 
 (defn calc-follower-move [leader-pos follower-pos]
   (let [vector-between (mapv - leader-pos follower-pos)]
