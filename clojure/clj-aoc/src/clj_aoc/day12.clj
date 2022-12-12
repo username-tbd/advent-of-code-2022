edd2279261e25ea76652529cd6d9577907427660
username-tbd <username-tbd-0@proton.me>
2022-12-12 16:38:55 -0600

    clj12: Part 2

--- clojure/clj-aoc/src/clj_aoc/day12.clj
+++ clojure/clj-aoc/src/clj_aoc/day12.clj
@@ -13,7 +13,7 @@
           :when (= character (get-in lines [row col]))]
       [row col])))
 
-(def start-ind-pt1 (find-char-ind \S))
+(def start-ind (find-char-ind \S))
 (def end-ind (find-char-ind \E))
 
 (defn char->elevation [character]
@@ -37,10 +37,10 @@
     (filterv #(<= (get-in heightmap %) (inc (get-in heightmap ind)))
              adjacent-inds)))
 
-(defn get-fewest-steps 
+(defn get-fewest-steps-map
   "Builds a map from index to fewest steps from start-ind.
   Covers all indices (we don't stop at the ending index)."
-  [heightmap start-ind]
+  [heightmap start-ind legal-moves-fn]
   (loop [fewest-steps {start-ind 0}
          i 1]
     (let [current-inds
@@ -48,7 +48,7 @@
                (filter (fn [[k v]] (= (dec i) v)))
                (map first))
           new-legal-moves
-          (->> (map get-legal-move-inds current-inds)
+          (->> (map legal-moves-fn current-inds)
                (reduce into #{})
                (filter #(not-any? #{%} (keys fewest-steps))))]
       (if (empty? new-legal-moves)
@@ -56,5 +56,31 @@
         (recur (reduce #(assoc %1 %2 i) fewest-steps new-legal-moves)
                (inc i))))))
 
-(def fewest-steps (get-fewest-steps heightmap start-ind-pt1))
-(println (fewest-steps end-ind))
+(def fewest-steps-map
+  (get-fewest-steps-map heightmap start-ind get-legal-move-inds))
+(println (fewest-steps-map end-ind))
+
+;; ----------
+;; Part 2
+;; We take the inverse of the "legal move" rule, and start at end-ind.
+;; Then find the closest ground-level index.
+
+(defn get-legal-move-inds-inverse [ind]
+  (let [adjacent-inds (get-adjacent-inds ind)]
+    (filterv #(>= (get-in heightmap %) (dec (get-in heightmap ind)))
+             adjacent-inds)))
+
+(def fewest-steps-map-inverse
+  (get-fewest-steps-map heightmap end-ind get-legal-move-inds-inverse))
+
+(def ground-level-inds
+  (for [row (range nrow)
+        col (range ncol)
+        :when (= 1 (get-in heightmap [row col]))]
+    [row col]))
+
+(->> fewest-steps-map-inverse
+     (filter #(some #{(first %)} ground-level-inds))
+     (sort-by second)
+     first
+     println)
