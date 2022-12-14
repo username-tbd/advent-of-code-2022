54715fa62032145e44153c671e5f7ecaa293ec9b
username-tbd <username-tbd-0@proton.me>
2022-12-14 12:19:05 -0600

    clj14: Change data structure from matrix to map

--- clojure/clj-aoc/src/clj_aoc/day14.clj
+++ clojure/clj-aoc/src/clj_aoc/day14.clj
@@ -2,8 +2,7 @@
   (:require [clj-aoc.util :as u])
   (:gen-class))
 
-;; Lots of parsing here. Get the [x,y] coordinate of every individual rock,
-;; then convert these coords to [row,col] indices out of which we make a matrix.
+(def sand-source-coord [500 0])
 
 (defn text-line->coord-vec [line]
   (->> (clojure.string/split line #" -> ")
@@ -33,44 +32,32 @@
        (reduce into [])
        distinct))
 
-(def sand-source-coord [500 0])
-(def all-coords (conj rock-coords sand-source-coord))
+(def lowest-y (apply max (map second rock-coords)))
 
-;; Now create the matrix that represents our cave.
-(def min-x (apply min (map first all-coords)))
-(def min-y (apply min (map second all-coords)))
-
-(defn coord->ind [[x y]]
-  [(- y min-y) (- x min-x)])
-
-(def rock-inds (mapv coord->ind rock-coords))
-(def sand-source-ind (coord->ind sand-source-coord))
-(def all-inds (conj rock-inds sand-source-ind))
-
-; Now we build the actual cave of air and rock
 (def cave
-  (let [n-rows (inc (apply max (map first rock-inds)))
-        n-cols (inc (apply max (map second rock-inds)))
-        repeatv (comp vec repeat)
-        empty-cave (repeatv n-rows (repeatv n-cols :air))]
-    (reduce #(assoc-in %1 %2 :rock) empty-cave rock-inds)))
+  (-> (reduce #(assoc %1 %2 :rock) {} rock-coords)
+      (assoc sand-source-coord :sand-source)))
 
 ;; ----------
 ;; Part one
 
+(defn into-the-void [coord]
+  (> (second coord) lowest-y))
+
 (defn simulate-unit-falling [cave]
-  (loop [[row col] sand-source-ind]
-    (let [down (get-in cave [(inc row) col])
-          down-left (get-in cave [(inc row) (dec col)])
-          down-right (get-in cave [(inc row) (inc col)])]
+  (loop [[x y] sand-source-coord]
+    (let [down [x (inc y)]
+          down-left [(dec x) (inc y)]
+          down-right [(inc x) (inc y)]]
       (cond
-        (= :air down) (recur [(inc row) col])
-        (nil? down) nil ; Out-of-bounds, endless void
-        (= :air down-left) (recur [(inc row) (dec col)])
-        (nil? down-left) nil
-        (= :air down-right) (recur [(inc row) (inc col)])
-        (nil? down-right) nil
-        :else (assoc-in cave [row col] :sand)))))
+        (nil? (cave down)) 
+        (if-not (into-the-void down) (recur down)) ; Else nil!
+        (nil? (cave down-left)) 
+        (if-not (into-the-void down-left) (recur down-left))
+        (nil? (cave down-right)) 
+        (if-not (into-the-void down-right) (recur down-right))
+        :else
+        (assoc cave [x y] :sand)))))
 
 (defn pour-sand [cave]
   (loop [cave cave]
@@ -79,6 +66,6 @@
       cave)))
 
 (->> (pour-sand cave)
-     flatten
-     (filter #(= :sand %))
-     count)
+     (filter #(= :sand (val %)))
+     count
+     println)
